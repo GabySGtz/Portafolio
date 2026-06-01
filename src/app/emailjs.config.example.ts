1d13cdec7568b181423b60c6bc6144bd98db2f4e
sanchez.gtz20 <sanchez.gtz20@gmail.com>
2026-06-01 12:58:06 -0600

    config

--- src/app/emailjs.config.example.ts
+++ src/app/emailjs.config.example.ts
@@ -1,5 +1,5 @@
 export const emailJsConfig = {
-  serviceId: 'TU_SERVICE_ID',
-  templateId: 'TU_TEMPLATE_ID',
-  publicKey: 'TU_PUBLIC_KEY',
+  serviceId: 'service_4xwo2ga',
+  templateId: 'template_rhh9mev',
+  publicKey: 'tc71iSSUuY7Z7ZCT2',
 };
